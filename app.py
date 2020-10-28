78adcd194b3646ed31a36d6a01c6b02c6f065055
d0dders <dodders@outlook.com>
2020-10-28 21:03:39 +0000

    Added main menu and team selection menu

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 import constants
+import sys
 
+MAIN_MENU = """
+BASKETBALL TEAM STATS TOOL
+
+---- MENU----
+
+Here are your choices:
+1) Display Team Stats
+2) Quit
+
+Enter an option >"""
 
 def clean_data(source_data):
     """ Takes in the PLAYERS constant
@@ -33,10 +44,41 @@ def balance_teams(source_teams, players):
     return(teams)
 
 
+def show_team_menu(teams):
+    """ Prints list of teams with indexes
+    Prompts users to choose which teams stats to display
+    """
+    team_menu_choice = True
+    while team_menu_choice:    
+        print("\n")
+        index = 1
+        for team in teams:
+            print(f"{index}) {team['team']}")
+            index += 1
+        try:
+            team_menu_choice = int(input("\nEnter an option >"))
+            if team_menu_choice > 0 and team_menu_choice <= len(teams):
+                team_choice = input("Team Stats")
+                show_team_stats(team_menu_choice - 1)
+            else:
+                raise ValueError()
+        except ValueError:
+            print("Invalid choice, try again")
+
+
+
 if __name__ == "__main__":
     
     players = clean_data(constants.PLAYERS)
     teams = balance_teams(constants.TEAMS, players)
-    for team in teams:
-        print(team)
-        print("----------------")
+ 
+    main_menu_choice = True
+    while main_menu_choice:
+        main_menu_choice = input(MAIN_MENU)
+        if main_menu_choice == '1':
+            show_team_menu(teams)
+        elif main_menu_choice == '2':
+            main_menu_choice = False
+        else: 
+            print("You did not make a valid selection")
+
